aa5b8745defa87fb84796f87b2bdd69b0b5a8404
TiamDev <fatimabakar707@gmail.com>
2024-07-19 21:20:30 +0300

    moving the modal up

--- src/components/Dashboard/taskbox/Task.jsx
+++ src/components/Dashboard/taskbox/Task.jsx
@@ -1,38 +1,12 @@
 import React, { useContext, useState } from "react";
 import "./taskbox.css";
-import image from "./../../../assets/image/mochi-post-it-notes (1).png";
-import image2 from "./../../../assets/image/mochi-exclamation-marks.png";
 import { TaskContext } from "../../../contexts/TaskContext";
-import { Modal, Button } from "react-bootstrap";
-const Task = ({
-  id,
-  title,
-  description,
-  date,
-  startTime,
-  endTime,
-  priority = "important",
-  isComplete,
-}) => {
+const Task = ({ task, showDelete, showEdit }) => {
   const { taskData, setTaskData } = useContext(TaskContext);
-  const [editTask, setEditTask] = useState({
-    title: title,
-    description: description,
-    startTime: startTime,
-    date: date,
-    endTime: endTime,
-    priority: priority,
-  });
 
-  const [showEdit, setShowEdit] = useState(false);
-  const [showDelete, setShowDelete] = useState(false);
-  const handleEditClose = () => setShowEdit(false);
-  const handleDeleteClose = () => setShowDelete(false);
-  const handleEditShow = () => setShowEdit(true);
-  const handleDeleteShow = () => setShowDelete(true);
   const handleCheckClick = () => {
     const updatedTasks = taskData.map((task) => {
-      if (task.id === id) {
+      if (task.id === task.id) {
         task.isComplete = !task.isComplete;
       }
       return task;
@@ -40,33 +14,12 @@ const Task = ({
     setTaskData(updatedTasks);
     localStorage.setItem("tasks", JSON.stringify(updatedTasks));
   };
-  const handleEditClick = () => {
-    const updatedTasks = taskData.map((task) => {
-      if (task.id === id) {
-        return {
-          ...task,
-          title: editTask.title,
-          description: editTask.description,
-          date: editTask.date,
-          priority: editTask.priority,
-          startTime: editTask.startTime,
-          endTime: editTask.endTime,
-        };
-      }
-      return task;
-    });
-    setTaskData(updatedTasks);
-    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
 
-    handleEditClose();
-  };
   const handleDeleteClick = () => {
-    const updatedTasks = taskData.filter((task) => {
-      return task.id !== id;
-    });
-    handleDeleteClose();
-    setTaskData(updatedTasks);
-    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+    showDelete(task);
+  };
+  const handleEditClick = () => {
+    showEdit(task);
   };
   function convertTo12HourFormat(time24) {
     let [hours, minutes] = time24.split(":");
@@ -99,13 +52,13 @@ const Task = ({
             </button>
             <ul className="dropdown-menu dropdown-menu-dark">
               <li>
-                <button className="dropdown-item " onClick={handleEditShow}>
+                <button className="dropdown-item " onClick={handleEditClick}>
                   <h6>Edit</h6>
                   <i className="bi bi-vector-pen"></i>
                 </button>
               </li>
               <li>
-                <button className="dropdown-item " onClick={handleDeleteShow}>
+                <button className="dropdown-item " onClick={handleDeleteClick}>
                   <h6>Delete</h6>
                   <i className="bi bi-trash3"></i>
                 </button>
@@ -114,7 +67,9 @@ const Task = ({
           </div>
         </span>
         <div
-          className={`card-body clickable-card  ${isComplete ? "checked" : ""}`}
+          className={`card-body clickable-card  ${
+            task.isComplete ? "checked" : ""
+          }`}
           onClick={handleCheckClick}
         >
           <span className="done-box">
@@ -126,223 +81,30 @@ const Task = ({
               type="checkbox"
               name="check"
               id="check"
-              checked={isComplete}
+              checked={task.isComplete}
               onChange={() => {}}
               style={{ display: "none" }}
             />
-            {title}
+            {task.title}
           </h5>
           <div className=" ps-4">
             {" "}
-            <h6 className="card-text">{description}</h6>
+            <h6 className="card-text">{task.description}</h6>
             <div className="row">
-              <span className={`priority ${priority}`}>
+              <span className={`priority ${task.priority}`}>
                 <i className="bi bi-flag-fill  pe-1"></i>
-                {priority}
+                {task.priority}
               </span>
             </div>
             <div className="row">
               <span className="col time">
-                {convertTo12HourFormat(startTime)} -{" "}
-                {convertTo12HourFormat(endTime)}
+                {convertTo12HourFormat(task.startTime)} -{" "}
+                {convertTo12HourFormat(task.endTime)}
               </span>
             </div>
           </div>
         </div>
       </div>
-      {/* Edit Modal  */}
-      <Modal
-        show={showEdit}
-        onHide={handleEditClose}
-        className="modal fade "
-        id="edit"
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>
-            <img src={image} width={"35rem"} className="me-3" alt="" />
-            Edit Task
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div className="mt-2">
-            <label htmlFor="title">Title</label>
-            <input
-              type="text"
-              name="title"
-              value={editTask.title}
-              onChange={(e) =>
-                setEditTask({ ...editTask, [e.target.name]: e.target.value })
-              }
-              className="form-control"
-            />
-          </div>
-          <div className="mt-2">
-            <label htmlFor="title">Description</label>
-            <textarea
-              className="form-control"
-              value={editTask.description}
-              name="description"
-              onChange={(e) =>
-                setEditTask({ ...editTask, [e.target.name]: e.target.value })
-              }
-            ></textarea>
-          </div>
-          <div className="row">
-            <div className="col-lg-4 col-sm-12">
-              <div className="">
-                <label htmlFor="date">Date</label>
-                <input
-                  type="datetime-date"
-                  name="date"
-                  value={editTask.date}
-                  onChange={(e) =>
-                    setEditTask({
-                      ...editTask,
-                      [e.target.name]: e.target.value,
-                    })
-                  }
-                  className="form-control"
-                />
-              </div>
-            </div>
-            <div className="col-lg-4 col-sm-6">
-              <div className="mt-2">
-                <label htmlFor="startTime">Starts</label>
-                <input
-                  type="time"
-                  name="startTime"
-                  value={editTask.startTime}
-                  onChange={(e) =>
-                    setEditTask({
-                      ...editTask,
-                      [e.target.name]: e.target.value,
-                    })
-                  }
-                  className="form-control"
-                />
-              </div>
-            </div>
-            <div className="col-lg-4 col-sm-6">
-              <div className="mt-2">
-                <label htmlFor="title">Endes</label>
-                <input
-                  type="time"
-                  name="endTime"
-                  value={editTask.endTime}
-                  onChange={(e) =>
-                    setEditTask({
-                      ...editTask,
-                      [e.target.name]: e.target.value,
-                    })
-                  }
-                  className="form-control"
-                />
-              </div>
-            </div>
-            <fieldset className="row mb-3 mt-2">
-              <label className=" ">Priority</label>
-              <div className="col-sm-10  mt-2">
-                <div className="d-flex justify-content-between">
-                  <div
-                    className={`form-check  ${
-                      editTask.priority === "necessary" ? "border__success" : ""
-                    }`}
-                  >
-                    <label htmlFor="necessary">
-                      <input
-                        className="form-check-input"
-                        type="radio"
-                        name="priority"
-                        id="necessary"
-                        value="necessary"
-                        checked={editTask.priority === "necessary"}
-                        onChange={(e) =>
-                          setEditTask({
-                            ...editTask,
-                            [e.target.name]: e.target.value,
-                          })
-                        }
-                      />
-                      Necessary
-                    </label>
-                  </div>
-                  <div
-                    className={`form-check ${
-                      editTask.priority === "important" ? "border__success" : ""
-                    }`}
-                  >
-                    <label htmlFor="important">
-                      <input
-                        className="form-check-input"
-                        type="radio"
-                        name="priority"
-                        id="important"
-                        value="important"
-                        checked={editTask.priority === "important"}
-                        onChange={(e) =>
-                          setEditTask({
-                            ...editTask,
-                            [e.target.name]: e.target.value,
-                          })
-                        }
-                      />
-                      Important
-                    </label>
-                  </div>
-                  <div
-                    className={`form-check ${
-                      editTask.priority === "normal" ? "border__success" : ""
-                    }`}
-                  >
-                    <label htmlFor="normal">
-                      <input
-                        className="form-check-input"
-                        type="radio"
-                        name="priority"
-                        id="normal"
-                        value="normal"
-                        checked={editTask.priority === "normal"}
-                        onChange={(e) =>
-                          setEditTask({
-                            ...editTask,
-                            [e.target.name]: e.target.value,
-                          })
-                        }
-                      />
-                      Normal
-                    </label>
-                  </div>
-                </div>
-              </div>
-            </fieldset>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={handleEditClick} className="btn btn-primary">
-            Save changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-      {/* Delete Modal  */}
-      <Modal
-        show={showDelete}
-        onHide={handleDeleteClose}
-        className="modal fade "
-        id="delete"
-      >
-        <Modal.Header closeButton>
-          <h1 className="modal-title fs-5" id="exampleModalLabel">
-            <img src={image2} width={"30rem"} className="me-3" alt="" />
-            Delete Task
-          </h1>
-        </Modal.Header>
-        <Modal.Body>Are you sure you want to delete ?</Modal.Body>
-        <Modal.Footer>
-          <Button onClick={handleDeleteClick} className="btn btn-primary">
-            Delete
-          </Button>
-        </Modal.Footer>
-      </Modal>
     </>
   );
 };
--- src/components/Dashboard/taskbox/TaskBox.jsx
+++ src/components/Dashboard/taskbox/TaskBox.jsx
@@ -1,16 +1,67 @@
 import React, { useContext, useState, useEffect, useMemo } from "react";
 import "./taskbox.css";
+import image from "./../../../assets/image/mochi-post-it-notes (1).png";
+import image2 from "./../../../assets/image/mochi-exclamation-marks.png";
+
 import TaskBoxHeader from "./TaskBoxHeader";
 import Task from "./Task";
 import { TaskContext } from "../../../contexts/TaskContext";
 import { SelectedDateContext } from "../../../contexts/SelectedDate";
 import { SearchContext } from "../../../contexts/SearchContext";
+import { Button, Modal } from "react-bootstrap";
 
 const TaskBox = ({ user }) => {
   const { selectedDate } = useContext(SelectedDateContext);
   const { search, setSearch } = useContext(SearchContext);
-  const { taskData } = useContext(TaskContext);
+  const { taskData, setTaskData } = useContext(TaskContext);
   const [displayType, setDisplayType] = useState("all");
+  const [modalTask, setModalTask] = useState("");
+
+  /* modal*/
+
+  const [showDelete, setShowDelete] = useState(false);
+  const handleDeleteShow = (task) => {
+    setModalTask(task);
+    setShowDelete(true);
+  };
+  const handleDeleteClose = () => setShowDelete(false);
+  const handleDeleteClick = () => {
+    const updatedTasks = taskData.filter((task) => {
+      return task.id !== modalTask.id;
+    });
+    handleDeleteClose();
+    setTaskData(updatedTasks);
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+  };
+
+  const [showEdit, setShowEdit] = useState(false);
+  const handleEditClose = () => setShowEdit(false);
+  const handleEditShow = (task) => {
+    setModalTask(task);
+    setShowEdit(true);
+    console.log(modalTask);
+  };
+  const handleEditClick = () => {
+    const updatedTasks = taskData.map((task) => {
+      if (task.id === modalTask.id) {
+        return {
+          ...task,
+          title: modalTask.title,
+          description: modalTask.description,
+          date: modalTask.date,
+          priority: modalTask.priority,
+          startTime: modalTask.startTime,
+          endTime: modalTask.endTime,
+        };
+      }
+      return task;
+    });
+    setTaskData(updatedTasks);
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+    handleEditClose();
+  };
+  /* ===modal===*/
+
   const changeDisplayType = (e) => {
     setDisplayType(e);
   };
@@ -51,15 +102,9 @@ const TaskBox = ({ user }) => {
           key={task.id}
         >
           <Task
-            key={task.id}
-            id={task.id}
-            title={task.title}
-            date={task.date}
-            description={task.description}
-            priority={task.priority}
-            startTime={task.startTime}
-            endTime={task.endTime}
-            isComplete={task.isComplete}
+            task={task}
+            showDelete={handleDeleteShow}
+            showEdit={handleEditShow}
           ></Task>
         </div>
       );
@@ -78,6 +123,203 @@ const TaskBox = ({ user }) => {
       ) : (
         <div className="row scroller">{taskList}</div>
       )}
+      {/* Delete Modal  */}
+      <Modal
+        show={showDelete}
+        onHide={handleDeleteClose}
+        className="modal fade "
+        id="delete"
+      >
+        <Modal.Header closeButton>
+          <h1 className="modal-title fs-5" id="exampleModalLabel">
+            <img src={image2} width={"30rem"} className="me-3" alt="" />
+            Delete Task
+          </h1>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to delete ?</Modal.Body>
+        <Modal.Footer>
+          <Button onClick={handleDeleteClick} className="btn btn-primary">
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      {/* Edit Modal  */}
+      <Modal
+        show={showEdit}
+        onHide={handleEditClose}
+        className="modal fade "
+        id="edit"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <img src={image} width={"35rem"} className="me-3" alt="" />
+            Edit Task
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="mt-2">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              name="title"
+              value={modalTask.title}
+              onChange={(e) =>
+                setModalTask({ ...modalTask, [e.target.name]: e.target.value })
+              }
+              className="form-control"
+            />
+          </div>
+          <div className="mt-2">
+            <label htmlFor="title">Description</label>
+            <textarea
+              className="form-control"
+              value={modalTask.description}
+              name="description"
+              onChange={(e) =>
+                setModalTask({ ...modalTask, [e.target.name]: e.target.value })
+              }
+            ></textarea>
+          </div>
+          <div className="row">
+            <div className="col-lg-4 col-sm-12">
+              <div className="">
+                <label htmlFor="date">Date</label>
+                <input
+                  type="datetime-date"
+                  name="date"
+                  value={modalTask.date}
+                  onChange={(e) =>
+                    setModalTask({
+                      ...modalTask,
+                      [e.target.name]: e.target.value,
+                    })
+                  }
+                  className="form-control"
+                />
+              </div>
+            </div>
+            <div className="col-lg-4 col-sm-6">
+              <div className="mt-2">
+                <label htmlFor="startTime">Starts</label>
+                <input
+                  type="time"
+                  name="startTime"
+                  value={modalTask.startTime}
+                  onChange={(e) =>
+                    setModalTask({
+                      ...modalTask,
+                      [e.target.name]: e.target.value,
+                    })
+                  }
+                  className="form-control"
+                />
+              </div>
+            </div>
+            <div className="col-lg-4 col-sm-6">
+              <div className="mt-2">
+                <label htmlFor="title">Endes</label>
+                <input
+                  type="time"
+                  name="endTime"
+                  value={modalTask.endTime}
+                  onChange={(e) =>
+                    setModalTask({
+                      ...modalTask,
+                      [e.target.name]: e.target.value,
+                    })
+                  }
+                  className="form-control"
+                />
+              </div>
+            </div>
+            <fieldset className="row mb-3 mt-2">
+              <label className=" ">Priority</label>
+              <div className="col-sm-10  mt-2">
+                <div className="d-flex justify-content-between">
+                  <div
+                    className={`form-check  ${
+                      modalTask.priority === "necessary"
+                        ? "border__success"
+                        : ""
+                    }`}
+                  >
+                    <label htmlFor="necessary">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="priority"
+                        id="necessary"
+                        value="necessary"
+                        checked={modalTask.priority === "necessary"}
+                        onChange={(e) =>
+                          setModalTask({
+                            ...modalTask,
+                            [e.target.name]: e.target.value,
+                          })
+                        }
+                      />
+                      Necessary
+                    </label>
+                  </div>
+                  <div
+                    className={`form-check ${
+                      modalTask.priority === "important"
+                        ? "border__success"
+                        : ""
+                    }`}
+                  >
+                    <label htmlFor="important">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="priority"
+                        id="important"
+                        value="important"
+                        checked={modalTask.priority === "important"}
+                        onChange={(e) =>
+                          setModalTask({
+                            ...modalTask,
+                            [e.target.name]: e.target.value,
+                          })
+                        }
+                      />
+                      Important
+                    </label>
+                  </div>
+                  <div
+                    className={`form-check ${
+                      modalTask.priority === "normal" ? "border__success" : ""
+                    }`}
+                  >
+                    <label htmlFor="normal">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="priority"
+                        id="normal"
+                        value="normal"
+                        checked={modalTask.priority === "normal"}
+                        onChange={(e) =>
+                          setModalTask({
+                            ...modalTask,
+                            [e.target.name]: e.target.value,
+                          })
+                        }
+                      />
+                      Normal
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </fieldset>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={handleEditClick} className="btn btn-primary">
+            Save changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
